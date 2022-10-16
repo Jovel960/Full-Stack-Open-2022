09cd89e79abfa0590c7165cc5e09a57fe77405d4
Jovel960 <jovel960@icloud.com>
2022-10-16 13:45:30 +0300

    Complete exercise 1.4 and fixed the object issue, now its ok !

--- part1/src/App.js
+++ part1/src/App.js
@@ -16,14 +16,15 @@ const Header = (props) => {
 };
 
 const Content = (props) => {
+  console.log(props.info);
   return (
     <div>
-      <Part value={props.part1} />
-      <Part value={props.exercise1} />
-      <Part value={props.part2} />
-      <Part value={props.exercise2} />
-      <Part value={props.part3} />
-      <Part value={props.exercise3} />
+      <Part value={props.info[0].name} />
+      <Part value={props.info[0].exercises} />
+      <Part value={props.info[1].name} />
+      <Part value={props.info[1].exercises} />
+      <Part value={props.info[2].name} />
+      <Part value={props.info[2].exercises} />
     </div>
   );
 };
@@ -41,31 +42,26 @@ const App = () => {
   const course = "Half Stack application development";
   const parts = [
     {
-      name: 'Fundamentals of React',
-      exercises: 10
+      name: "Fundamentals of React",
+      exercises: 10,
     },
     {
-      name: 'Using props to pass data',
-      exercises: 7
+      name: "Using props to pass data",
+      exercises: 7,
     },
     {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+      name: "State of a component",
+      exercises: 14,
+    },
+  ];
 
   return (
     <div>
       <Header course={course} />
-      <Content
-        part1={parts[0].name}
-        part2={parts[1].name}
-        part3={parts[2].name}
-        exercise1={parts[0].exercises}
-        exercise2={parts[1].exercises}
-        exercise3={parts[2].exercises}
+      <Content info={parts} />
+      <Total
+        sumOfEx={parts[0].exercises + parts[1].exercises + parts[2].exercises}
       />
-      <Total sumOfEx={parts[0].exercises+parts[1].exercises+parts[2].exercises} />
     </div>
   );
 };
