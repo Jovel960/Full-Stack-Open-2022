4377d895fbb0fa2f7dacfcc600370c98ea84f19b
Jovel960 <jovel960@icloud.com>
2022-10-17 12:14:55 +0300

    last changes before part 1d

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,69 +1,20 @@
-const Part = (props) => {
-  console.log(props.value);
-  return (
-    <div>
-      <p>{props.value}</p>
-    </div>
-  );
-};
+import { useState } from "react";
+const Display = ({counter}) => <div>{counter}</div>
 
-const Header = (props) => {
-  return (
-    <div>
-      <Part value={props.course} />
-    </div>
-  );
-};
-
-const Content = (props) => {
-  console.log(props.info);
-  return (
-    <div>
-      <Part value={props.info[0].name} />
-      <Part value={props.info[0].exercises} />
-      <Part value={props.info[1].name} />
-      <Part value={props.info[1].exercises} />
-      <Part value={props.info[2].name} />
-      <Part value={props.info[2].exercises} />
-    </div>
-  );
-};
-
-const Total = (props) => {
-  return (
-    <div>
-      <p>Sum of exercises : </p>
-      <Part value={props.sumOfEx} />
-    </div>
-  );
-};
+const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  const [counter, setCounter] = useState(0);
+  const handleReset = () => setCounter(0);
+  const handleClick = () => setCounter(counter + 1);
+  const handleMinus = () => setCounter(counter - 1);
 
   return (
     <div>
-      <Header course={course.name} />
-      <Content info={course.parts} />
-      <Total
-        sumOfEx={course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises}
-      />
+      <Display counter={counter} />
+      <Button onClick={handleClick} text="Click Me!" />
+      <Button onClick={handleReset} text="Reset!" />
+      <Button onClick={handleMinus} text="Minus by 1" />
     </div>
   );
 };
