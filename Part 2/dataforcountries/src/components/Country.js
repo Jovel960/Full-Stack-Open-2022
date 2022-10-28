95d0ffc574741ee2e490ed449b61da3db7beec26
Jovel960 <jovel960@icloud.com>
2022-10-28 20:45:45 +0300

    Complete task 2.14, before running this task make sure you have api key and run this command REACT_APP_API_KEY=key  npm start

--- Part 2/dataforcountries/src/components/Country.js
+++ Part 2/dataforcountries/src/components/Country.js
@@ -1,20 +1,41 @@
 import Button from "./Button";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import CountryClicked from "./CountryClicked";
+import axios from "axios";
 const Country = ({ countryinfo, num }) => {
   const [countryClicked, setCountryClicked] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [weather, setWeather] = useState([]);
+  const [windSpeed, setWindSpeed] = useState([]);
+  const [srcImg, setSrcImg] = useState("");
+  const api_key = process.env.REACT_APP_API_KEY;
   const countryLang = [];
   for (const [key, value] of Object.entries(countryinfo.languages)) {
     countryLang.push(value);
   }
+  const weatherToKelvin = (kelvin) => kelvin - 273.15;
+  useEffect(() => {
+    axios
+      .get(
+        `http://api.openweathermap.org/data/2.5/weather?q=${countryinfo.capital}&APPID=${api_key}`
+      )
+      .then((res) => {
+        setWeather((Math.round(weatherToKelvin(res.data.main.temp)) * 10) / 10);
+        setWindSpeed(res.data.wind.speed);
+        setSrcImg(res.data.weather[0].icon)
+        //console.log("1", res.data.weather[0].icon);
+      })
+      .catch((err) =>
+        setErrorMessage("Weather is unavialible for this country")
+      );
+  }, []);
   const onClick = () => setCountryClicked(!countryClicked);
   //console.log(countryinfo);
   if (!num)
     return (
       <div>
         <p>
-          {countryinfo.name.common}{" "}
-          <Button onClick={onClick} />
+          {countryinfo.name.common} <Button onClick={onClick} />
         </p>
         <div>
           <CountryClicked countryinfo={countryinfo} flag={countryClicked} />
@@ -40,9 +61,28 @@ const Country = ({ countryinfo, num }) => {
         <div>
           <img src={`${countryinfo.flags.png}`} alt="Flag" />
         </div>
+        <div>
+          <div>
+            {errorMessage === "" ? (
+              <>
+                <h3 style={{ fontWeight: "bold" }}>
+                  Weatehr in {countryinfo.name.common}
+                </h3>
+                <p style={{ fontWeight: "bold" }}>
+                  Temperature {weather} Celcius
+                </p>
+                <img src={`http://openweathermap.org/img/wn/${srcImg}@2x.png`} />
+                <p style={{ fontWeight: "bold" }}>Wind {windSpeed} m/s</p>
+              </>
+            ) : (
+              <>
+                <p>{errorMessage}</p>
+              </>
+            )}
+          </div>
+        </div>
       </div>
     );
   }
 };
-
 export default Country;
