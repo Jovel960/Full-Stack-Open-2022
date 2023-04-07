f54a4fa2924eb35d9cf88f995fc05eacf545831c
Jovel960 <jovel960@icloud.com>
2023-04-07 16:36:55 +0300

    complete 4.8

--- Part 4/bloglist/tests/apiBlogsList.test.js
+++ Part 4/bloglist/tests/apiBlogsList.test.js
@@ -5,11 +5,36 @@ const app = require("../app");
 
 const api = supertest(app);
 
+const initialBlogs = [
+  {
+    title:"Biggest rappers in the world",
+    author:"Biggie",
+    url:"",
+    likes:15
+  },
+  {
+    title:"The next israeli war",
+    author:"Amit Atzmon",
+    url:"",
+    likes:30
+  }
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({});
+
+  let firstBlog = new Blog(initialBlogs[0]);
+  await firstBlog.save();
+
+  let secondBlog = new Blog(initialBlogs[1]);
+  await secondBlog.save();
+})
+
 test('blogs are returned as json', async () => {
  const response = await api.get('/api/blogs')
     console.log(response)
     expect(response.status).toBe(200);
-    expect(response.body).toHaveLength(1);
+    expect(response.body).toHaveLength(2);
 }, 10000)
 
 afterAll(async () => {
