e64d28a9c904fd0d7d2bd0a88216e2ea8ce1b9d8
Jovel960 <jovel960@icloud.com>
2023-04-11 18:54:30 +0300

    added development config

--- Part 4/bloglist/utilits/config.js
+++ Part 4/bloglist/utilits/config.js
@@ -1,9 +1,14 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const PORT = process.env.PORT
-const MONGODB_URI = process.env.NODE_ENV === "testing" ? process.env.TEST_MONGODB_URI : process.env.MONGODB_URI;
+const PORT = process.env.PORT;
+const MONGODB_URI =
+  process.env.NODE_ENV === "testing"
+    ? process.env.TEST_MONGODB_URI
+    : process.env.NODE_ENV === "development"
+    ? process.env.DEVELOPMENT_MONGODB_URI
+    : process.env.MONGODB_URI;
 
 module.exports = {
   MONGODB_URI,
-  PORT
-}
+  PORT,
+};
