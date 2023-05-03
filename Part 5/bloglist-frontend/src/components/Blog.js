f92367d23082ff924a8848bb667729abf5b23eda
Jovel960 <jovel960@icloud.com>
2023-05-03 22:52:01 +0300

    done 5.8

--- Part 5/bloglist-frontend/src/components/Blog.js
+++ Part 5/bloglist-frontend/src/components/Blog.js
@@ -1,13 +1,15 @@
 import Togglable from "./Togglable";
 
 const Blog = ({ blog }) => {
+  console.log("blog", blog)
   return (
-    <div style={{ flexDirection: "column" }}>
+    <div style={{ flexDirection: "column", cursor:"default" }}>
       {blog.title}
       <Togglable buttonLabel="View">
-        <p>{blog.url}</p>
-        <p>{blog.likes}</p>
-        <p>{blog.author}</p>
+        <p>{"URL: " + blog.url}</p>
+        <p>{"Likes: " + blog.likes}</p>
+        <p>{"Author: " + blog.author}</p>
+        <p>{"Owner: " + blog.user.name}</p>
       </Togglable>
     </div>
   );
