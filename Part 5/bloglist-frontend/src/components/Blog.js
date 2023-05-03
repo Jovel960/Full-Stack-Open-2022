674e58ac18d15266234f9fb7e7d1a769c74f0e46
Jovel960 <jovel960@icloud.com>
2023-05-03 23:40:40 +0300

    updates

--- Part 5/bloglist-frontend/src/components/Blog.js
+++ Part 5/bloglist-frontend/src/components/Blog.js
@@ -1,13 +1,43 @@
 import Togglable from "./Togglable";
-
+import blogServices from "../services/blogs";
 const Blog = ({ blog }) => {
-  console.log("blog", blog)
+  const likeBlog = async () => {
+    try {
+      await blogServices.likeBlog(blog.id);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  const deleteBlog = async () => {
+    try {
+      await blogServices.deleteBlog(blog.id);
+    } catch (e) {
+      console.log(e);
+    }
+  };
   return (
-    <div style={{ flexDirection: "column", cursor:"default" }}>
-      {blog.title}
+    <div style={{ flexDirection: "column", cursor: "default" }}>
+      <span style={{ display: "flex", alignItems:"center", justifyItems:"center" }}>
+        <p>{blog.title}</p>
+        <button
+          style={{ color: "blue", textAlign: "center", borderRadius: "8px", height:"min-content", marginLeft:"10px" }}
+          onClick={deleteBlog}
+        >
+          Delete
+        </button>
+      </span>
+
       <Togglable buttonLabel="View">
         <p>{"URL: " + blog.url}</p>
-        <p>{"Likes: " + blog.likes}</p>
+        <span style={{ display: "flex", alignItems:"center", justifyItems:"center"  }}>
+          <p>{"Likes: " + blog.likes}</p>
+          <button
+            style={{ color: "blue", textAlign: "center", borderRadius: "8px", height:"min-content", marginLeft:"10px", }}
+            onClick={likeBlog}
+          >
+            Like
+          </button>
+        </span>
         <p>{"Author: " + blog.author}</p>
         <p>{"Owner: " + blog.user.name}</p>
       </Togglable>
--- Part 5/bloglist-frontend/src/services/blogs.js
+++ Part 5/bloglist-frontend/src/services/blogs.js
@@ -7,19 +7,37 @@ const setToken = (UserToken) => {
   token = `Bearer ${UserToken}`;
 };
 
+const deleteBlog = async (id) => {
+  const config = {
+    headers: { Authorization: token },
+  };
+  let data = await axios.delete(`${baseUrl}/${id}`, config);
+  return data;
+};
+
 const getAll = () => {
-    const request = axios.get(baseUrl);
-    return request.then((response) => response.data);
+  const request = axios.get(baseUrl);
+  return request.then((response) => response.data);
 };
 
 const createBlog = async (newBlog) => {
-   const config = {
+  const config = {
     headers: { Authorization: token },
-  }
-  console.log(config)
-  let data = await axios.post(baseUrl, newBlog, config)
+  };
+  console.log(config);
+  let data = await axios.post(baseUrl, newBlog, config);
   return data;
-}
+};
+
+const likeBlog = async (id) => {
+  console.log(id);
+  const config = {
+    headers: { Authorization: token },
+  };
+  let data = await axios.put(`${baseUrl}/${id}`, config);
+  console.log(data, id);
+  return data;
+};
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default { getAll, setToken, createBlog };
+export default { getAll, setToken, createBlog, likeBlog , deleteBlog};
